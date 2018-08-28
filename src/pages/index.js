1ef91d022708272cc0f27acf29fcbe3abb3ec692
monika patel <monikapatel.it@outlook.com>
2018-08-29 11:33:15 +1200

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h3>Hi Campers,</h3>
+	<h1>I am Monika Patel.</h1>
+    <p>Front End Developer - Auckland New Zealand</p>
+    
+	<p><a href="https://www.linkedin.com/in/monikapatelit/" rel="nofollow">LinkedIN</a></p>
+   
+  </div>
+)
+
+export default IndexPage
